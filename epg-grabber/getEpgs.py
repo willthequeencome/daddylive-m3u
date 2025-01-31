202b232a536efd2b653daf723638fb8157afe11f
willthequeencome <161780156+willthequeencome@users.noreply.github.com>
2025-01-31 18:10:51 -0500

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -66,7 +66,7 @@ def filter_and_build_epg(urls):
 m3u4u_epg = os.getenv("M3U4U_EPG")
 
 urls = [
-	'https://www.bevy.be/generate/8TbvgWSctM.xml.gz',
+	'https://www.open-epg.com/files/saudiarabia1.xml.gz',
        'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
         'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
 	'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
